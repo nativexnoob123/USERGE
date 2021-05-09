9ff94292e4cb4cb1583a428c09fd554bf413a912
nativexnoob123 <82684674+nativexnoob123@users.noreply.github.com>
2021-05-10 02:38:03 +0530

    Update gban.py

--- userge/plugins/admin/gban.py
+++ userge/plugins/admin/gban.py
@@ -154,34 +154,20 @@ async def gban_user(message: Message):
     channels_creator = 0
     try:
         async for dialog in userge.iter_dialogs():
-            unread_mentions += dialog.unread_mentions_count
-            unread_msg += dialog.unread_messages_count
-            chat_type = dialog.chat.type
-            if chat_type in ["bot", "private"]:
-                private_chats += 1
-                if chat_type == "bot":
-                    bots += 1
-                else:
-                    users_ += 1
+            try:
+                is_admin = await admin_check(dialog.chat.id, owner.id)
+                is_creator = dialog.chat.is_creator
+            except UserNotParticipant:
+                is_admin = False
+                is_creator = False
+            if chat_type in ["group", "supergroup"]:
+                groups += 1
+                if is_admin:
+                    groups_admin += 1
             else:
-                try:
-                    is_admin = await admin_check(dialog.chat.id, owner.id)
-                    is_creator = dialog.chat.is_creator
-                except UserNotParticipant:
-                    is_admin = False
-                    is_creator = False
-                if chat_type in ["group", "supergroup"]:
-                    groups += 1
-                    if is_admin:
-                        groups_admin += 1
-                    if is_creator:
-                        groups_creator += 1
-                else:  # Channel
-                    channels += 1
-                    if is_admin:
-                        channels_admin += 1
-                    if is_creator:
-                        channels_creator += 1
+                channels += 1
+                if is_admin:
+                    channels_admin += 1
     except FloodWait as e:
         await asyncio.sleep(e.x + 5)
     await message.edit(
